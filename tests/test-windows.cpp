e6e2adf58c719f4bcf62143fbdca520467c1766e
Fred Emmott <git@fredemmott.com>
2025-12-13 07:20:52 -0600

    ✅ Add tests for windows error reporting
    refs #25

--- tests/test-windows.cpp
+++ tests/test-windows.cpp
@@ -12,44 +12,95 @@
 #include "output.hpp"
 
 namespace TestWindows {
-struct MyArgs {
-  std::string mFoo;
-  std::string mBar;
-  std::string mBaz;
-  std::string mEmpty;
-};
-}// namespace TestWindows
+    struct MyArgs {
+        std::string mFoo;
+        std::string mBar;
+        std::string mBaz;
+        std::string mEmpty;
+    };
+} // namespace TestWindows
 using namespace TestWindows;
 
 TEST_CASE("wWinMain", "[windows]") {
-  // wWinMain gives us the command line all in one UTF-16 string
-  constexpr auto commandLine
-    = L"test_app --foo 💩 --bar \"Dzień dobry\" --empty \"\" --baz test";
+    // wWinMain gives us the command line all in one UTF-16 string
+    constexpr auto commandLine
+            = L"test_app --foo 💩 --bar \"Dzień dobry\" --empty \"\" --baz test";
 
-  Output out, err;
-  const auto args = magic_args::parse<MyArgs>(commandLine, {}, out, err);
-  CHECK(out.empty());
-  CHECK(err.empty());
-  REQUIRE(args.has_value());
-  CHECK(args->mFoo == "💩");
-  CHECK(args->mBar == "Dzień dobry");
-  CHECK(args->mEmpty.empty());
-  CHECK(args->mBaz == "test");
+    Output out, err;
+    const auto args = magic_args::parse<MyArgs>(commandLine, {}, out, err);
+    CHECK(out.empty());
+    CHECK(err.empty());
+    REQUIRE(args.has_value());
+    CHECK(args->mFoo == "💩");
+    CHECK(args->mBar == "Dzień dobry");
+    CHECK(args->mEmpty.empty());
+    CHECK(args->mBaz == "test");
 }
 
 TEST_CASE("winMain", "[windows]") {
-  REQUIRE(GetACP() == CP_UTF8);
-  // wWinMain gives us the command line all in one active-code-page string
-  constexpr auto commandLine
-    = "test_app --foo 💩 --bar \"Dzień dobry\" --empty \"\" --baz test";
+    REQUIRE(GetACP() == CP_UTF8);
+    // wWinMain gives us the command line all in one active-code-page string
+    constexpr auto commandLine
+            = "test_app --foo 💩 --bar \"Dzień dobry\" --empty \"\" --baz test";
 
-  Output out, err;
-  const auto args = magic_args::parse<MyArgs>(commandLine, {}, out, err);
-  CHECK(out.empty());
-  CHECK(err.get() == "");
-  REQUIRE(args.has_value());
-  CHECK(args->mFoo == "💩");
-  CHECK(args->mBar == "Dzień dobry");
-  CHECK(args->mEmpty.empty());
-  CHECK(args->mBaz == "test");
+    Output out, err;
+    const auto args = magic_args::parse<MyArgs>(commandLine, {}, out, err);
+    CHECK(out.empty());
+    CHECK(err.get() == "");
+    REQUIRE(args.has_value());
+    CHECK(args->mFoo == "💩");
+    CHECK(args->mBar == "Dzień dobry");
+    CHECK(args->mEmpty.empty());
+    CHECK(args->mBaz == "test");
+}
+
+// The intent is to check that error reporting is plumbed in, not specifically *this* error
+TEST_CASE("wWinMain - invalid argument", "[windows]") {
+    constexpr auto commandLine
+            = L"test_app --invalid-argument";
+    Output out, err;
+    const auto args = magic_args::parse<MyArgs>(commandLine, {}, out, err);
+    CHECK(!args.has_value());
+
+    CHECK(out.empty());
+    CHECK(err.get() == &R"EOF(
+test_app: Unrecognized option: --invalid-argument
+
+Usage: test_app [OPTIONS...]
+
+Options:
+
+      --foo=VALUE
+      --bar=VALUE
+      --baz=VALUE
+      --empty=VALUE
+
+  -?, --help                   show this message
+)EOF"[1]);
+}
+
+// The intent is to check that error reporting is plumbed in, not specifically *this* error
+TEST_CASE("winMain - invalid argument", "[windows]") {
+    REQUIRE(GetACP() == CP_UTF8);
+    constexpr auto commandLine
+            = "test_app --invalid-argument";
+    Output out, err;
+    const auto args = magic_args::parse<MyArgs>(commandLine, {}, out, err);
+    CHECK(!args.has_value());
+
+    CHECK(out.empty());
+    CHECK(err.get() == &R"EOF(
+test_app: Unrecognized option: --invalid-argument
+
+Usage: test_app [OPTIONS...]
+
+Options:
+
+      --foo=VALUE
+      --bar=VALUE
+      --baz=VALUE
+      --empty=VALUE
+
+  -?, --help                   show this message
+)EOF"[1]);
 }
