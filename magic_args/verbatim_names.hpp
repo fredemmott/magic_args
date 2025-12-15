ea6058001d6337a49199005f20fc811058e16576
Fred Emmott <git@fredemmott.com>
2025-12-15 06:55:16 -0600

    🎨 Default the parent for verbatim_names

--- magic_args/verbatim_names.hpp
+++ magic_args/verbatim_names.hpp
@@ -3,12 +3,13 @@
 #pragma once
 
 #ifndef MAGIC_ARGS_SINGLE_FILE
+#include "detail/concepts.hpp"
 #include "gnu_style_parsing_traits.hpp"
 #endif
 
 namespace magic_args::inline public_api {
 
-template <class T>
+template <parsing_traits T = gnu_style_parsing_traits>
 struct verbatim_names : T {
   static void normalize_option_name(std::string&) {};
   static void normalize_positional_argument_name(std::string&) {};
